6571f9305d85052cede0e145fd14f6934d2e019c
TheFrederick <thefrederick@seznam.cz>
2021-09-12 19:11:45 +0200

    Add files via upload

---
+++ scripts/copyText.js
@@ -0,0 +1,8 @@
+// Copy hard-coded text to clipboard
+function copyDiscord() {
+    navigator.clipboard.writeText("TheFrederick#8776");
+    prompt("Tag copied to clipboard!", "TheFrederick#8776");
+}
+
+// Just 4 Fun
+document.addEventListener('contextmenu', event => event.preventDefault());
