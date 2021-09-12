03ec506f4febe283f6a3be8ec90a5dc6a4864024
TheFrederick <thefrederick@seznam.cz>
2021-09-12 21:36:54 +0200

    Update and rename copyText.js to Fun.js

---
+++ scripts/Fun.js
@@ -0,0 +1,2 @@
+// Just 4 Fun
+document.addEventListener('contextmenu', event => event.preventDefault());
--- scripts/copyText.js
+++
@@ -1,8 +0,0 @@
-// Copy hard-coded text to clipboard
-function copyDiscord() {
-    navigator.clipboard.writeText("TheFrederick#8776");
-    prompt("Tag copied to clipboard!", "TheFrederick#8776");
-}
-
-// Just 4 Fun
-document.addEventListener('contextmenu', event => event.preventDefault());
